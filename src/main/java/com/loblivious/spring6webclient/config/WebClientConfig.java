15406fa472d87599096436ff822cc9b8ef225397
Yen-Jung Luo <loblivious.ee@gmail.com>
2025-11-12 13:56:15 +0800

    webclient global configuration

---
+++ src/main/java/com/loblivious/spring6webclient/config/WebClientConfig.java
@@ -0,0 +1,21 @@
+package com.loblivious.spring6webclient.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
+
+@Configuration
+public class WebClientConfig implements WebClientCustomizer {
+
+  private final String rootUrl;
+
+  public WebClientConfig(@Value("${beer.host.rootUrl}") String rootUrl) {
+    this.rootUrl = rootUrl;
+  }
+
+  @Override
+  public void customize(Builder webClientBuilder) {
+    webClientBuilder.baseUrl(this.rootUrl);
+  }
+}
