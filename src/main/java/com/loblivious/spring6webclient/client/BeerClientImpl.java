737f0938aa9edcbbd2264bb5cb03823e094d20c0
Yen-Jung Luo <loblivious.ee@gmail.com>
2025-11-12 13:39:06 +0800

    fixed an error

--- src/main/java/com/loblivious/spring6webclient/client/BeerClientImpl.java
+++ src/main/java/com/loblivious/spring6webclient/client/BeerClientImpl.java
@@ -24,19 +24,19 @@ public class BeerClientImpl implements BeerClient {
 
   @Override
   public Flux<String> listBeer() {
-    return webClient.get().uri(BEER_PATH, String.class)
+    return webClient.get().uri(BEER_PATH)
         .retrieve().bodyToFlux(String.class);
   }
 
   @Override
   public Flux<Map> listBeerMap() {
-    return webClient.get().uri(BEER_PATH, Map.class)
+    return webClient.get().uri(BEER_PATH)
         .retrieve().bodyToFlux(Map.class);
   }
 
   @Override
   public Flux<JsonNode> listBeerJsonNode() {
-    return webClient.get().uri(BEER_PATH, JsonNode.class)
+    return webClient.get().uri(BEER_PATH)
         .retrieve().bodyToFlux(JsonNode.class);
   }
 
