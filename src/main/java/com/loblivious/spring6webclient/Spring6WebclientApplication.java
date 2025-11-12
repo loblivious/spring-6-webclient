eb47fe0a004fa0012608089250395cedaa8fc80d
Yen-Jung Luo <loblivious.ee@gmail.com>
2025-11-12 10:00:21 +0800

    project init

---
+++ src/main/java/com/loblivious/spring6webclient/Spring6WebclientApplication.java
@@ -0,0 +1,13 @@
+package com.loblivious.spring6webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebclientApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(Spring6WebclientApplication.class, args);
+  }
+
+}
