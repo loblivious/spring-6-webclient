8e31cb5610001bba91bb9b1e852ccd87a5eae1e8
Yen-Jung Luo <loblivious.ee@gmail.com>
2025-11-12 13:36:21 +0800

    removed extra lines

--- src/main/java/com/loblivious/spring6webclient/model/BeerDTO.java
+++ src/main/java/com/loblivious/spring6webclient/model/BeerDTO.java
@@ -12,17 +12,10 @@ public class BeerDTO {
 
   private UUID id;
   private Integer version;
-
   private String beerName;
-
-
   private BeerStyle beerStyle;
-
-
   private String upc;
   private Integer quantityOnHand;
-
-
   private BigDecimal price;
   private LocalDateTime createdDate;
   private LocalDateTime updateDate;
